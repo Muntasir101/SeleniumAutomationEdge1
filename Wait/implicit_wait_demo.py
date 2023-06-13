35f455df9b75d256545f38d6fb956e8520497c7e
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2023-06-13 16:04:32 +0600

    wait

--- Wait/implicit_wait_demo.py
+++ Wait/implicit_wait_demo.py
@@ -1,3 +1,11 @@
+"""
+1. Wait for a specified amount of time for an element to appear
+before throwing an exception(NoSuchElementException).
+2. Used for when there are certain elements on page that take varying
+times to load.
+3. This is global wait time that applies to all elements.
+"""
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -9,6 +17,8 @@ def test_login_valid():
 
     # Step 2: Open URL
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+    driver.implicitly_wait(10)
 
     # Step 3: Find Username
     username = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
@@ -29,32 +39,4 @@ def test_login_valid():
     login_button.click()
 
 
-def test_login_invalid():
-    # Step 1: Browser Launch
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-
-    # Step 2: Open URL
-    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-
-    # Step 3: Find Username
-    username = driver.find_element(By.CSS_SELECTOR, "input[name='username']")
-
-    # Step 4: Find Password
-    password = driver.find_element(By.CSS_SELECTOR, "input[name='password']")
-
-    # Step 5: Fine Login Button
-    login_button = driver.find_element(By.CSS_SELECTOR, ".orangehrm-login-button")
-
-    # Step 6: Enter Username
-    username.send_keys("Admin123")
-
-    # Step 7: Enter Password
-    password.send_keys("admin123")
-
-    # Step 8: Click Login Button
-    login_button.click()
-
-
 test_login_valid()
-test_login_invalid()
